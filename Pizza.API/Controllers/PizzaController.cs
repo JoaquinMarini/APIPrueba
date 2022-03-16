521f4aa9e3ffb40fd1848fe63e5444bd60d1593d
JoaquinMarini <101258852+JoaquinMarini@users.noreply.github.com>
2022-03-16 10:42:02 -0300

    Add files via upload

---
+++ Pizza.API/Controllers/PizzaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Pizza.API.Models;
+
+namespace Pizza.API.Controllers
+{
+    [ApiController]
+    [Route("/api/[controller]")]
+    public class PizzaController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<PizzaController> _logger;
+
+        public PizzaController(ILogger<PizzaController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Pizzas> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Pizzas
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ Pizza.API/Models/Pizza.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Pizza.API.Models
+{
+    public class Pizzas
+    {
+        public DateTime Date{ get; set; }
+        public int TemperatureC { get; set; }
+        public int TemperatureF { get; set; }
+    }
+}
